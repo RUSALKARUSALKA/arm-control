a8df0f2cbc8e694dcd26fddf8682b9368a2eeecd
dxg <dxg@xx.com>
2024-07-24 21:31:47 +0800

    Add moveit folder as part of the main repository

---
+++ moveit/moveit_task_constructor/core/src/solvers/pipeline_planner.cpp
@@ -0,0 +1,240 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Bielefeld University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Bielefeld University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Robert Haschke
+   Desc:    plan using MoveIt's PlanningPipeline
+*/
+
+#include <moveit/task_constructor/solvers/pipeline_planner.h>
+#include <moveit/task_constructor/task.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_pipeline/planning_pipeline.h>
+#include <moveit_msgs/msg/motion_plan_request.hpp>
+#include <moveit/kinematic_constraints/utils.h>
+
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <tf2_eigen/tf2_eigen.h>
+#endif
+
+namespace moveit {
+namespace task_constructor {
+namespace solvers {
+
+PipelinePlanner::PipelinePlanner(const rclcpp::Node::SharedPtr& node, const std::string& pipeline_name,
+                                 const std::string& planner_id)
+  : PipelinePlanner(node, [&]() {
+	  std::unordered_map<std::string, std::string> pipeline_id_planner_id_map;
+	  pipeline_id_planner_id_map[pipeline_name] = planner_id;
+	  return pipeline_id_planner_id_map;
+  }()) {}
+
+PipelinePlanner::PipelinePlanner(
+    const rclcpp::Node::SharedPtr& node, const std::unordered_map<std::string, std::string>& pipeline_id_planner_id_map,
+    const std::unordered_map<std::string, planning_pipeline::PlanningPipelinePtr>& planning_pipelines,
+    const moveit::planning_pipeline_interfaces::StoppingCriterionFunction& stopping_criterion_callback,
+    const moveit::planning_pipeline_interfaces::SolutionSelectionFunction& solution_selection_function)
+  : node_(node)
+  , last_successful_planner_("")
+  , pipeline_id_planner_id_map_(pipeline_id_planner_id_map)
+  , stopping_criterion_callback_(stopping_criterion_callback)
+  , solution_selection_function_(solution_selection_function) {
+	// If the pipeline name - pipeline map is passed as constructor argument, use it. Otherwise, it will be created in
+	// the init(..) function
+	if (!planning_pipelines.empty()) {
+		planning_pipelines_ = planning_pipelines;
+	}
+	// Declare properties of the MotionPlanRequest
+	properties().declare<uint>("num_planning_attempts", 1u, "number of planning attempts");
+	properties().declare<moveit_msgs::msg::WorkspaceParameters>(
+	    "workspace_parameters", moveit_msgs::msg::WorkspaceParameters(), "allowed workspace of mobile base?");
+
+	properties().declare<double>("goal_joint_tolerance", 1e-4, "tolerance for reaching joint goals");
+	properties().declare<double>("goal_position_tolerance", 1e-4, "tolerance for reaching position goals");
+	properties().declare<double>("goal_orientation_tolerance", 1e-4, "tolerance for reaching orientation goals");
+	// Declare properties that configure the planning pipeline
+	properties().declare<std::unordered_map<std::string, std::string>>(
+	    "pipeline_id_planner_id_map", std::unordered_map<std::string, std::string>(),
+	    "Set of pipelines and planners used for planning");
+}
+
+bool PipelinePlanner::setPlannerId(const std::string& pipeline_name, const std::string& planner_id) {
+	// Only set ID if pipeline exists. It is not possible to create new pipelines with this command.
+	if (pipeline_id_planner_id_map_.count(pipeline_name) == 0) {
+		RCLCPP_ERROR(node_->get_logger(),
+		             "PipelinePlanner does not have a pipeline called '%s'. Cannot set pipeline ID '%s'",
+		             pipeline_name.c_str(), planner_id.c_str());
+		return false;
+	}
+	pipeline_id_planner_id_map_[pipeline_name] = planner_id;
+	return true;
+}
+
+void PipelinePlanner::setStoppingCriterionFunction(
+    const moveit::planning_pipeline_interfaces::StoppingCriterionFunction& stopping_criterion_function) {
+	stopping_criterion_callback_ = stopping_criterion_function;
+}
+void PipelinePlanner::setSolutionSelectionFunction(
+    const moveit::planning_pipeline_interfaces::SolutionSelectionFunction& solution_selection_function) {
+	solution_selection_function_ = solution_selection_function;
+}
+
+void PipelinePlanner::init(const core::RobotModelConstPtr& robot_model) {
+	// If no planning pipelines exist, create them based on the pipeline names provided in pipeline_id_planner_id_map_.
+	// The assumption here is that all parameters required by the planning pipeline can be found in a namespace that
+	// equals the pipeline name.
+	if (planning_pipelines_.empty()) {
+		planning_pipelines_ = moveit::planning_pipeline_interfaces::createPlanningPipelineMap(
+		    [&]() {
+			    // Create pipeline name vector from the keys of pipeline_id_planner_id_map_
+			    if (pipeline_id_planner_id_map_.empty()) {
+				    throw std::runtime_error("Cannot initialize PipelinePlanner: No planning pipeline was provided and "
+				                             "pipeline_id_planner_id_map_ is empty!");
+			    }
+
+			    std::vector<std::string> pipeline_names;
+			    for (const auto& pipeline_name_planner_id_pair : pipeline_id_planner_id_map_) {
+				    pipeline_names.push_back(pipeline_name_planner_id_pair.first);
+			    }
+			    return pipeline_names;
+		    }(),
+		    robot_model, node_);
+	}
+
+	// Check if it is still empty
+	if (planning_pipelines_.empty()) {
+		throw std::runtime_error(
+		    "Cannot initialize PipelinePlanner: Could not create any valid entries for planning pipeline maps!");
+	}
+}
+
+MoveItErrorCode PipelinePlanner::plan(const planning_scene::PlanningSceneConstPtr& from,
+                                      const planning_scene::PlanningSceneConstPtr& to,
+                                      const moveit::core::JointModelGroup* joint_model_group, double timeout,
+                                      robot_trajectory::RobotTrajectoryPtr& result,
+                                      const moveit_msgs::msg::Constraints& path_constraints) {
+	// Construct goal constraints from the goal planning scene
+	const auto goal_constraints = kinematic_constraints::constructGoalConstraints(
+	    to->getCurrentState(), joint_model_group, properties().get<double>("goal_joint_tolerance"));
+	return plan(from, joint_model_group, goal_constraints, timeout, result, path_constraints);
+}
+
+MoveItErrorCode PipelinePlanner::plan(const planning_scene::PlanningSceneConstPtr& from,
+                                      const moveit::core::LinkModel& link, const Eigen::Isometry3d& offset,
+                                      const Eigen::Isometry3d& target_eigen,
+                                      const moveit::core::JointModelGroup* joint_model_group, double timeout,
+                                      robot_trajectory::RobotTrajectoryPtr& result,
+                                      const moveit_msgs::msg::Constraints& path_constraints) {
+	last_successful_planner_.clear();
+
+	// Construct a Cartesian target pose from the given target transform and offset
+	geometry_msgs::msg::PoseStamped target;
+	target.header.frame_id = from->getPlanningFrame();
+	target.pose = tf2::toMsg(target_eigen * offset.inverse());
+
+	const auto goal_constraints = kinematic_constraints::constructGoalConstraints(
+	    link.getName(), target, properties().get<double>("goal_position_tolerance"),
+	    properties().get<double>("goal_orientation_tolerance"));
+
+	return plan(from, joint_model_group, goal_constraints, timeout, result, path_constraints);
+}
+
+MoveItErrorCode PipelinePlanner::plan(const planning_scene::PlanningSceneConstPtr& planning_scene,
+                                      const moveit::core::JointModelGroup* joint_model_group,
+                                      const moveit_msgs::msg::Constraints& goal_constraints, double timeout,
+                                      robot_trajectory::RobotTrajectoryPtr& result,
+                                      const moveit_msgs::msg::Constraints& path_constraints) {
+	// Create a request for every planning pipeline that should run in parallel
+	std::vector<moveit_msgs::msg::MotionPlanRequest> requests;
+	requests.reserve(pipeline_id_planner_id_map_.size());
+
+	auto const property_pipeline_id_planner_id_map =
+	    properties().get<std::unordered_map<std::string, std::string>>("pipeline_id_planner_id_map");
+	for (auto const& pipeline_id_planner_id_pair :
+	     (!property_pipeline_id_planner_id_map.empty() ? property_pipeline_id_planner_id_map :
+                                                        pipeline_id_planner_id_map_)) {
+		// Check that requested pipeline exists and skip it if it doesn't exist
+		if (planning_pipelines_.find(pipeline_id_planner_id_pair.first) == planning_pipelines_.end()) {
+			RCLCPP_WARN(
+			    node_->get_logger(),
+			    "Pipeline '%s' is not available of this PipelineSolver instance. Skipping a request for this pipeline.",
+			    pipeline_id_planner_id_pair.first.c_str());
+			continue;
+		}
+		// Create MotionPlanRequest for pipeline
+		moveit_msgs::msg::MotionPlanRequest request;
+		request.pipeline_id = pipeline_id_planner_id_pair.first;
+		request.group_name = joint_model_group->getName();
+		request.planner_id = pipeline_id_planner_id_pair.second;
+		request.allowed_planning_time = timeout;
+		request.start_state.is_diff = true;  // we don't specify an extra start state
+		request.num_planning_attempts = properties().get<uint>("num_planning_attempts");
+		request.max_velocity_scaling_factor = properties().get<double>("max_velocity_scaling_factor");
+		request.max_acceleration_scaling_factor = properties().get<double>("max_acceleration_scaling_factor");
+		request.workspace_parameters = properties().get<moveit_msgs::msg::WorkspaceParameters>("workspace_parameters");
+		request.goal_constraints.resize(1);
+		request.goal_constraints.at(0) = goal_constraints;
+		request.path_constraints = path_constraints;
+		requests.push_back(request);
+	}
+
+	// Run planning pipelines in parallel to create a vector of responses. If a solution selection function is provided,
+	// planWithParallelPipelines will return a vector with the single best solution
+	std::vector<::planning_interface::MotionPlanResponse> responses =
+	    moveit::planning_pipeline_interfaces::planWithParallelPipelines(
+	        requests, planning_scene, planning_pipelines_, stopping_criterion_callback_, solution_selection_function_);
+
+	// If solutions exist and the first one is successful
+	if (!responses.empty()) {
+		auto const solution = responses.at(0);
+		if (solution) {
+			// Choose the first solution trajectory as response
+			result = solution.trajectory;
+			last_successful_planner_ = solution.planner_id;
+			if (!bool(result))  // If the plan was not a success
+			{
+				return MoveItErrorCode(solution.error_code.val, solution.error_code.message, solution.error_code.source);
+			}
+			return MoveItErrorCode(MoveItErrorCodes::SUCCESS);
+		}
+	}
+	return MoveItErrorCode(MoveItErrorCodes::FAILURE, "No solutions generated from Pipeline Planner");
+}
+std::string PipelinePlanner::getPlannerId() const {
+	return last_successful_planner_.empty() ? std::string("Unknown") : last_successful_planner_;
+}
+}  // namespace solvers
+}  // namespace task_constructor
+}  // namespace moveit
---
+++ moveit/moveit_task_constructor/core/src/stages/connect.cpp
@@ -0,0 +1,276 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Bielefeld University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Bielefeld University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Michael Goerner, Robert Haschke
+   Desc:    Connect arbitrary states by motion planning
+*/
+
+#include <moveit/task_constructor/stages/connect.h>
+#include <moveit/task_constructor/merge.h>
+#include <moveit/task_constructor/cost_terms.h>
+
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/trajectory_processing/time_optimal_trajectory_generation.h>
+
+using namespace trajectory_processing;
+
+namespace moveit {
+namespace task_constructor {
+namespace stages {
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("Connect");
+
+Connect::Connect(const std::string& name, const GroupPlannerVector& planners) : Connecting(name), planner_(planners) {
+	setTimeout(1.0);
+	setCostTerm(std::make_unique<cost::PathLength>());
+
+	auto& p = properties();
+	p.declare<MergeMode>("merge_mode", WAYPOINTS, "merge mode");
+	p.declare<moveit_msgs::msg::Constraints>("path_constraints", moveit_msgs::msg::Constraints(),
+	                                         "constraints to maintain during trajectory");
+	properties().declare<TimeParameterizationPtr>("merge_time_parameterization",
+	                                              std::make_shared<TimeOptimalTrajectoryGeneration>());
+}
+
+void Connect::reset() {
+	Connecting::reset();
+	merged_jmg_.reset();
+	subsolutions_.clear();
+	states_.clear();
+}
+
+void Connect::init(const core::RobotModelConstPtr& robot_model) {
+	Connecting::init(robot_model);
+
+	InitStageException errors;
+	if (planner_.empty())
+		errors.push_back(*this, "empty set of groups");
+
+	std::vector<const moveit::core::JointModelGroup*> groups;
+	for (const GroupPlannerVector::value_type& pair : planner_) {
+		if (!robot_model->hasJointModelGroup(pair.first))
+			errors.push_back(*this, "invalid group: " + pair.first);
+		else if (!pair.second)
+			errors.push_back(*this, "invalid planner for group: " + pair.first);
+		else {
+			pair.second->init(robot_model);
+
+			auto jmg = robot_model->getJointModelGroup(pair.first);
+			groups.push_back(jmg);
+		}
+	}
+
+	if (!errors && groups.size() >= 2 && !merged_jmg_) {  // enable merging?
+		try {
+			merged_jmg_.reset(task_constructor::merge(groups));
+		} catch (const std::runtime_error& e) {
+			RCLCPP_INFO_STREAM(LOGGER, this->name() << ": " << e.what() << ". Disabling merging.");
+		}
+	}
+
+	if (errors)
+		throw errors;
+}
+
+bool Connect::compatible(const InterfaceState& from_state, const InterfaceState& to_state) const {
+	if (!Connecting::compatible(from_state, to_state))
+		return false;
+
+	const moveit::core::RobotState& from = from_state.scene()->getCurrentState();
+	const moveit::core::RobotState& to = to_state.scene()->getCurrentState();
+
+	// compose set of joint names we plan for
+	std::set<std::string> planned_joint_names;
+	for (const GroupPlannerVector::value_type& pair : planner_) {
+		const moveit::core::JointModelGroup* jmg = from.getJointModelGroup(pair.first);
+		const auto& names = jmg->getJointModelNames();
+		planned_joint_names.insert(names.begin(), names.end());
+	}
+	// all active joints that we don't plan for should match
+	for (const moveit::core::JointModel* jm : from.getRobotModel()->getJointModels()) {
+		if (planned_joint_names.count(jm->getName()))
+			continue;  // ignore joints we plan for
+
+		const unsigned int num = jm->getVariableCount();
+		Eigen::Map<const Eigen::VectorXd> positions_from(from.getJointPositions(jm), num);
+		Eigen::Map<const Eigen::VectorXd> positions_to(to.getJointPositions(jm), num);
+		if (!(positions_from - positions_to).isZero(1e-4)) {
+			RCLCPP_INFO_STREAM(LOGGER, "Deviation in joint " << jm->getName() << ": [" << positions_from.transpose()
+			                                                 << "] != [" << positions_to.transpose() << "]");
+			return false;
+		}
+	}
+	return true;
+}
+
+void Connect::compute(const InterfaceState& from, const InterfaceState& to) {
+	const auto& props = properties();
+	double timeout = this->timeout();
+	MergeMode mode = props.get<MergeMode>("merge_mode");
+	const auto& path_constraints = props.get<moveit_msgs::msg::Constraints>("path_constraints");
+
+	const moveit::core::RobotState& final_goal_state = to.scene()->getCurrentState();
+	std::vector<PlannerIdTrajectoryPair> trajectory_planner_vector;
+
+	std::vector<planning_scene::PlanningSceneConstPtr> intermediate_scenes;
+	planning_scene::PlanningSceneConstPtr start = from.scene();
+	intermediate_scenes.push_back(start);
+
+	bool success = false;
+	std::string error_message = "";
+	std::vector<double> positions;
+	for (const GroupPlannerVector::value_type& pair : planner_) {
+		// set intermediate goal state
+		planning_scene::PlanningScenePtr end = start->diff();
+		const moveit::core::JointModelGroup* jmg = final_goal_state.getJointModelGroup(pair.first);
+		final_goal_state.copyJointGroupPositions(jmg, positions);
+		moveit::core::RobotState& goal_state = end->getCurrentStateNonConst();
+		goal_state.setJointGroupPositions(jmg, positions);
+		goal_state.update();
+		intermediate_scenes.push_back(end);
+
+		robot_trajectory::RobotTrajectoryPtr trajectory;
+		const auto planner_solution_status = pair.second->plan(start, end, jmg, timeout, trajectory, path_constraints);
+		if (bool(planner_solution_status)) {
+			success = true;
+		}
+
+		trajectory_planner_vector.push_back(PlannerIdTrajectoryPair({ pair.second->getPlannerId(), trajectory }));
+
+		if (!success) {
+			error_message = planner_solution_status.message;
+			break;
+		}
+
+		// continue from reached state
+		start = end;
+	}
+
+	SolutionBasePtr solution;
+	if (success && mode != SEQUENTIAL)  // try to merge
+		solution = merge(trajectory_planner_vector, intermediate_scenes, from.scene()->getCurrentState());
+	if (!solution)  // success == false or merging failed: store sequentially
+		solution = makeSequential(trajectory_planner_vector, intermediate_scenes, from, to);
+	if (!success)  // error during sequential planning
+		solution->markAsFailure(error_message);
+
+	connect(from, to, solution);
+}
+
+SolutionSequencePtr
+Connect::makeSequential(const std::vector<PlannerIdTrajectoryPair>& trajectory_planner_vector,
+                        const std::vector<planning_scene::PlanningSceneConstPtr>& intermediate_scenes,
+                        const InterfaceState& from, const InterfaceState& to) {
+	assert(!trajectory_planner_vector.empty());
+	assert(trajectory_planner_vector.size() + 1 == intermediate_scenes.size());
+
+	/* We need to decouple the sequence of subsolutions, created here, from the external from and to states.
+	   Hence, we create new interface states for all subsolutions. */
+	const InterfaceState* start = &*states_.insert(states_.end(), InterfaceState(from.scene()));
+
+	auto scene_it = intermediate_scenes.begin();
+	SolutionSequence::container_type sub_solutions;
+	for (const auto& pair : trajectory_planner_vector) {
+		// persistently store sub solution
+		auto inserted = subsolutions_.insert(
+		    subsolutions_.end(), SubTrajectory(pair.robot_trajectory_ptr, 0.0, std::string(""), pair.planner_name));
+		inserted->setCreator(this);
+		if (!pair.robot_trajectory_ptr)  // a null RobotTrajectoryPtr indicates a failure
+			inserted->markAsFailure();
+		// push back solution pointer
+		sub_solutions.push_back(&*inserted);
+
+		// create a new end state, either from intermediate or final planning scene
+		planning_scene::PlanningSceneConstPtr end_ps =
+		    (sub_solutions.size() < trajectory_planner_vector.size()) ? *++scene_it : to.scene();
+		const InterfaceState* end = &*states_.insert(states_.end(), InterfaceState(end_ps));
+
+		// provide newly created start/end states
+		subsolutions_.back().setStartState(*start);
+		subsolutions_.back().setEndState(*end);
+
+		start = end;  // end state becomes next start state
+	}
+
+	return std::make_shared<SolutionSequence>(std::move(sub_solutions));
+}
+
+SubTrajectoryPtr Connect::merge(const std::vector<PlannerIdTrajectoryPair>& trajectory_planner_vector,
+                                const std::vector<planning_scene::PlanningSceneConstPtr>& intermediate_scenes,
+                                const moveit::core::RobotState& state) {
+	// no need to merge if there is only a single sub trajectory
+	if (trajectory_planner_vector.size() == 1)
+		return std::make_shared<SubTrajectory>(trajectory_planner_vector.at(0).robot_trajectory_ptr, 0.0, std::string(""),
+		                                       trajectory_planner_vector.at(0).planner_name);
+
+	auto jmg = merged_jmg_.get();
+	assert(jmg);
+	auto timing = properties().get<TimeParameterizationPtr>("merge_time_parameterization");
+	robot_trajectory::RobotTrajectoryPtr merged_trajectory = task_constructor::merge(
+	    [&]() {
+		    // Move trajectories into single vector
+		    std::vector<robot_trajectory::RobotTrajectoryConstPtr> robot_traj_vector;
+		    robot_traj_vector.reserve(trajectory_planner_vector.size());
+
+		    // Copy second elements of robot planner vector into separate vector
+		    std::transform(begin(trajectory_planner_vector), end(trajectory_planner_vector),
+		                   std::back_inserter(robot_traj_vector),
+		                   [](auto const& pair) { return pair.robot_trajectory_ptr; });
+		    return robot_traj_vector;
+	    }(),
+	    state, jmg, *timing);
+
+	// check merged trajectory is empty or has collisions
+	if (!merged_trajectory ||
+	    !intermediate_scenes.front()->isPathValid(*merged_trajectory,
+	                                              properties().get<moveit_msgs::msg::Constraints>("path_constraints"))) {
+		return SubTrajectoryPtr();
+	}
+
+	// Copy first elements of robot planner vector into separate vector
+	std::vector<std::string> planner_names;
+	planner_names.reserve(trajectory_planner_vector.size());
+	std::transform(begin(trajectory_planner_vector), end(trajectory_planner_vector), std::back_inserter(planner_names),
+	               [](auto const& pair) { return pair.planner_name; });
+
+	// Create a sequence of planner names
+	std::string planner_name_sequence;
+	for (auto const& name : planner_names) {
+		planner_name_sequence += std::string(", " + name);
+	}
+	return std::make_shared<SubTrajectory>(merged_trajectory, 0.0, std::string(""), planner_name_sequence);
+}
+}  // namespace stages
+}  // namespace task_constructor
+}  // namespace moveit
